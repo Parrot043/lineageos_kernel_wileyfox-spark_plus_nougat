3afdd7f2d564807532fc6f74c97bbcd6d432cff1
xcore142 <samsung.fly64@gmail.com>
2018-03-08 18:28:48 +0300

    first commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Architecture
+export ARCH=arm64
+export TARGET_ARCH=arm64
+make O=out cyanogenmod_porridge_defconfig
+export KBUILD_BUILD_USER=bulkin042
+export KBUILD_BUILD_HOST=universal
+export CROSS_COMPILE=~/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+make O=out Image.gz-dtb
