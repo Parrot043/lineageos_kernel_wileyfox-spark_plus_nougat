9842a5d94372cd31d4251406fa09bb244820c3f7
fuldaros <litefros@gmail.com>
2018-03-09 23:21:43 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 export ARCH=arm64
 export TARGET_ARCH=arm64
 make O=out cyanogenmod_porridge_defconfig
-export KBUILD_BUILD_USER=bulkin042
-export KBUILD_BUILD_HOST=universal
+export KBUILD_BUILD_USER=fuldaros
+export KBUILD_BUILD_HOST=ESA
 export CROSS_COMPILE=~/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 make O=out Image.gz-dtb
