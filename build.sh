1ad2a88fecbf55c4f4fac255775b9821059441d2
fuldaros <litefros@gmail.com>
2018-03-31 00:54:14 +0300

    new struct

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-# Architecture
+# by fuldaros
+stamp=$(date +"%Y%m%d-%H%M");
+kernel=paperplane_"$stamp";
 export ARCH=arm64
 export TARGET_ARCH=arm64
-export KBUILD_BUILD_USER=PaperPlane
-export KBUILD_BUILD_HOST=fuldaros
-export CROSS_COMPILE=~/kernels/UBERTC5.3/bin/aarch64-linux-android-
-make O=../out Image.gz-dtb
-echo       COPY KERNEL       
-rm ../CarlivImageKitchen64/boot-alps/boot.img-kernel
-cat ../out/arch/arm64/boot/Image.gz-dtb > ../CarlivImageKitchen64/boot-alps/boot.img-kernel
-echo         SUCCES!         
-
+export KBUILD_BUILD_USER=fuldaros
+export KBUILD_BUILD_HOST=hhost
+pwd > pwd.dat
+read pwd < pwd.dat
+rm -f pwd.dat
+export CROSS_COMPILE="$pwd"/tools/bin/aarch64-linux-gnu-
+cd sources/
+make -j3 O=../out/paperplane Image.gz-dtb
+echo       COPY KERNEL
+echo       by fuldaros
+cat ../out/paperplane/arch/arm64/boot/Image.gz-dtb > ../outkernel/"$kernel"
+rm -rf ../out/paperplane/arch/arm64/boot/
---
+++ prebuilt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# by fuldaros
+rm -f gen.info
+mkdir out
+export ARCH=arm64
+stamp=$(date +"%H:%M:%S %Y.%m.%d");
+echo "generate by fuldaros's script on "$stamp" " > gen.info
+make O=../out/paperplane porridge_pp_defconfig
+./build.sh
