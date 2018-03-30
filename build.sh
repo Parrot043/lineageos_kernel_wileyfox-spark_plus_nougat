3294abb42fb98e9e2e1e1f1024fc1bd1901257d2
fuldaros <litefros@gmail.com>
2018-03-31 02:31:11 +0300

    add logb && fix

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # by fuldaros
 stamp=$(date +"%Y%m%d-%H%M");
 kernel=paperplane_"$stamp";
+logb=logb_"$stamp";
 export ARCH=arm64
 export TARGET_ARCH=arm64
 export KBUILD_BUILD_USER=fuldaros
@@ -11,8 +12,10 @@ read pwd < pwd.dat
 rm -f pwd.dat
 export CROSS_COMPILE="$pwd"/tools/bin/aarch64-linux-gnu-
 cd sources/
-make -j3 O=../out/paperplane Image.gz-dtb
-echo       COPY KERNEL
-echo       by fuldaros
+echo made by fuldaros
+echo "Подождите...ядро собирается :3"
+make -j3 O=../out/paperplane Image.gz-dtb > ../outkernel/"$logb"
+echo "Копирую ядро..."
 cat ../out/paperplane/arch/arm64/boot/Image.gz-dtb > ../outkernel/"$kernel"
 rm -rf ../out/paperplane/arch/arm64/boot/
+echo Success!
